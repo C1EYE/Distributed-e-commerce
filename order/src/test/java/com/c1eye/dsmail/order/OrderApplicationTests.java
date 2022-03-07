2c4454d320b6ebb7f4f312b296a8c9f358aa5264
c1eye <c1eyemmj@gmail.com>
2022-03-07 12:08:44 +0800

    init

---
+++ order/src/test/java/com/c1eye/dsmail/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c1eye.dsmail.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
