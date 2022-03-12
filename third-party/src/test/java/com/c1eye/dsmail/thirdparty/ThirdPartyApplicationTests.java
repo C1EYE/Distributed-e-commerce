9049e5aa1a666a58ef2eabd60d966d902044744a
c1eye <c1eyemmj@gmail.com>
2022-03-12 15:05:09 +0800

    完成商品管理模块

---
+++ third-party/src/test/java/com/c1eye/dsmail/thirdparty/ThirdPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c1eye.dsmail.thirdparty;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdPartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
